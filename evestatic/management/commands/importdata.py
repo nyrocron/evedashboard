08d076bc3edb357c440d38340855c0f30b90eb4a
Florian Tautz <dev@nyronet.de>
2013-06-15 13:11:50 +0200

    update model import code, replace omments with docstrings where appropriate

--- evestatic/management/commands/importdata.py
+++ evestatic/management/commands/importdata.py
@@ -14,74 +14,92 @@ class Command(NoArgsCommand):
         self._import_race()
         self._import_marketgroup()
         self._import_invcategory()
-        self.stdout.write("import done.")
+        self.stdout.write("Static data import done.")
     
-    def _import_map(self):
-        pass
-    
-    # import from chrRaces table:
-    # "raceID" integer NOT NULL, -> pk
-    # "raceName" varchar(100) DEFAULT NULL, -> name
-    # "description" varchar(1000) DEFAULT NULL, -> description
-    # "shortDescription" varchar(500) DEFAULT NULL, -> description_short
     def _import_race(self):
-        static_table = 'chrRaces'
-        static_cols = ['raceID', 'raceName', 'description', 'shortDescription']
-        model_params = ['pk', 'name', 'description', 'description_short']
-        data_transforms = {
-            'description': _string_null_to_empty,
-            'shortDescription': _string_null_to_empty,
-        }
-        self._import_data(Race, model_params, static_table, static_cols, data_transforms)
-    
-    # import from invMarketGroups table:
-    # "marketGroupID" integer NOT NULL, -> pk
-    # "parentGroupID" integer DEFAULT NULL, -> parent
-    # "marketGroupName" varchar(100) DEFAULT NULL, -> name
-    # "description" varchar(3000) DEFAULT NULL, -> description
-    # "hasTypes" integer DEFAULT NULL, -> has_types
-    def _import_marketgroup(self):
-        static_table = 'invMarketGroups'
-        static_cols = ['marketGroupID', 'parentGroupID', 'marketGroupName', 'description', 'hasTypes']
-        model_params = ['pk', 'parent_id', 'name', 'description', 'has_types']
-        data_transforms = {
-            'hasTypes': _int_to_bool,
-            'description': _string_null_to_empty,
-        }
-        self._import_data(MarketGroup, model_params, static_table, static_cols, data_transforms)
-
-    # import from invCategories table:
-    # "categoryID" integer NOT NULL, -> pk
-    # "categoryName" varchar(100) DEFAULT NULL, -> name
-    # "description" varchar(3000) DEFAULT NULL, -> description
-    # "published" integer DEFAULT NULL, -> published
-    def _import_invcategory(self):
-        static_table = 'invCategories'
-        static_cols = ['categoryID', 'categoryName', 'description', 'published']
-        model_params = ['pk', 'name', 'description', 'published']
-        data_transforms = {
-            'description': _string_null_to_empty,
-            'published': _int_to_bool,
-        }
-        self._import_data(InvCategory, model_params, static_table, static_cols, data_transforms)
-    
-    def _import_data(self, model, model_params, static_table, static_cols, data_transforms):
-        cursor_static = self._db_static.cursor()
-        cursor_static.execute("SELECT " + ",".join(static_cols) + " FROM " + static_table)
+        """ Import from chrRaces table.
         
+        "raceID" integer NOT NULL, -> pk
+        "raceName" varchar(100) DEFAULT NULL, -> name
+        "description" varchar(1000) DEFAULT NULL, -> description
+        "shortDescription" varchar(500) DEFAULT NULL, -> description_short
+        
+        """
+        self._import_data('chrRaces', Race, [
+            ('raceID', 'pk', None),
+            ('raceName', 'name', None),
+            ('description', 'description', _string_null_to_empty),
+            ('shortDescription', 'description_short', _string_null_to_empty),
+        ])
+    
+    def _import_marketgroup(self):
+        """ Import from invMarketGroups table.
+        
+        "marketGroupID" integer NOT NULL, -> pk
+        "parentGroupID" integer DEFAULT NULL, -> parent
+        "marketGroupName" varchar(100) DEFAULT NULL, -> name
+        "description" varchar(3000) DEFAULT NULL, -> description
+        "hasTypes" integer DEFAULT NULL, -> has_types
+    
+        """
+        self._import_data('invMarketGroups', MarketGroup, [
+            ('marketGroupID', 'pk', None),
+            ('parentGroupID', 'parent_id', None),
+            ('marketGroupName', 'name', None),
+            ('description', 'description', _string_null_to_empty),
+            ('hasTypes', 'has_types', _int_to_bool),
+        ])
+
+    def _import_invcategory(self):
+        """ Import from invCategories table.
+        
+        "categoryID" integer NOT NULL, -> pk
+        "categoryName" varchar(100) DEFAULT NULL, -> name
+        "description" varchar(3000) DEFAULT NULL, -> description
+        "published" integer DEFAULT NULL, -> published
+        
+        """
+        self._import_data('invCategories', InvCategory, [
+            ('categoryID', 'pk', None),
+            ('categoryName', 'name', None),
+            ('description', 'description', _string_null_to_empty),
+            ('published', 'published', _int_to_bool),
+        ])
+    
+    #def _import_invgroup(self):
+    #    """ Import from invGroups table into InvGroup model"""
+    #    static_table = 'invGroups'
+    #    static_cols = ['groupID', 'categoryID', 'groupName', 'description', 'useBasePrice', 'allowManufacture']
+    #    model_params = ['pk', 'name', 'description', 'published']
+    #    data_transforms = {
+    #        'description': _string_null_to_empty,
+    #        'published': _int_to_bool,
+    #    }
+    #    self._import_data(InvCategory, model_params, static_table, static_cols, data_transforms)
+    
+    def _import_data(self, static_table, model, col_map):
+        """ Import data from a static db table to a model"""
+        # query static db
+        cursor_static = self._db_static.cursor()
+        cursor_static.execute("SELECT " + ",".join([x[0] for x in col_map]) +
+                              " FROM " + static_table)
+        
+        # delete old values
         cursor_default = self._db_default.cursor()
         cursor_default.execute("DELETE FROM " + model._meta.db_table)
         
+        # from sql result create models, apply tranform if there is any,
+        # then save the created object
         for row in cursor_static.fetchall():
-            d = dict()
-            for i in range(len(row)):
-                if static_cols[i] in data_transforms:
-                    d[model_params[i]] = data_transforms[static_cols[i]](row[i])
+            model_values = dict()
+            for i in range(len(col_map)):
+                if col_map[i][2] is not None:
+                    model_values[col_map[i][1]] = col_map[i][2](row[i])
                 else:
-                    d[model_params[i]] = row[i]
-            model(**d).save()
+                    model_values[col_map[i][1]] = row[i]
+            model(**model_values).save()
         
-        self.stdout.write("imported %s -> %s" % (static_table, model.__name__))
+        self.stdout.write("Imported %s -> %s." % (static_table, model.__name__))
     
 def _string_null_to_empty(value):
     if value is None:
--- evestatic/models.py
+++ evestatic/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
-# equivalent of invGroups
-class InvGroup(models.Model):
-    name = models.CharField(max_length=100)
-    # TODO
 
-# equivalent of invCategories
-#
-# implemented:
-# "categoryID" integer NOT NULL, -> pk
-# "categoryName" varchar(100) DEFAULT NULL, -> name
-# "description" varchar(3000) DEFAULT NULL, -> description
-# "published" integer DEFAULT NULL, -> published
-# not implemented:
-# "iconID" integer DEFAULT NULL,
 class InvCategory(models.Model):
+    
+    """ Equivalent of invCategories
+
+    "categoryID" integer NOT NULL, -> pk
+    "categoryName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(3000) DEFAULT NULL, -> description
+    "published" integer DEFAULT NULL, -> published
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    
+    """
+    
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=3000)
     published = models.BooleanField()
@@ -22,17 +22,54 @@ class InvCategory(models.Model):
     def __str__(self):
         return self.name
 
-# equivalent of marketGroups
-#
-# implemented:
-# "marketGroupID" integer NOT NULL, -> pk
-# "parentGroupID" integer DEFAULT NULL, -> parent
-# "marketGroupName" varchar(100) DEFAULT NULL, -> name
-# "description" varchar(3000) DEFAULT NULL, -> description
-# "hasTypes" integer DEFAULT NULL, -> has_types
-# not implemented:
-# "iconID" integer DEFAULT NULL,
+class InvGroup(models.Model):
+    
+    """ Equivalent of invGroups
+    
+    "groupID" integer NOT NULL, -> pk
+    "categoryID" integer DEFAULT NULL, -> invcategory
+    "groupName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(3000) DEFAULT NULL, -> description
+    "useBasePrice" integer DEFAULT NULL, -> use_baseprice
+    "allowManufacture" integer DEFAULT NULL, -> allow_manufacture
+    "allowRecycler" integer DEFAULT NULL, -> allow_recycler
+    "anchored" integer DEFAULT NULL, -> anchored
+    "anchorable" integer DEFAULT NULL, -> anchorable
+    "fittableNonSingleton" integer DEFAULT NULL, -> fittable_non_singleton
+    "published" integer DEFAULT NULL, -> published
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    
+    """
+    
+    name = models.CharField(max_length=100)
+    invcategory = models.ForeignKey(InvCategory)
+    description = models.CharField(max_length=3000)
+    use_baseprice = models.BooleanField()
+    allow_manufacture = models.BooleanField()
+    allow_recycler = models.BooleanField()
+    anchored = models.BooleanField()
+    anchorable = models.BooleanField()
+    fittable_non_singleton = models.BooleanField()
+    published = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+
 class MarketGroup(models.Model):
+    """ Equivalent of marketGroups
+    
+    "marketGroupID" integer NOT NULL, -> pk
+    "parentGroupID" integer DEFAULT NULL, -> parent
+    "marketGroupName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(3000) DEFAULT NULL, -> description
+    "hasTypes" integer DEFAULT NULL, -> has_types
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    
+    """
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', null=True)
     description = models.CharField(max_length=3000)
@@ -41,16 +78,19 @@ class MarketGroup(models.Model):
     def __str__(self):
         return self.name
 
-# equivalent of chrRaces
-#
-# implemented:
-# "raceID" integer NOT NULL, -> pk
-# "raceName" varchar(100) DEFAULT NULL, -> name
-# "description" varchar(1000) DEFAULT NULL, -> description
-# "shortDescription" varchar(500) DEFAULT NULL, -> description_short
-# not implemented:
-# "iconID" integer DEFAULT NULL,
 class Race(models.Model):
+    """ Equivalent of chrRaces
+    
+    "raceID" integer NOT NULL, -> pk
+    "raceName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(1000) DEFAULT NULL, -> description
+    "shortDescription" varchar(500) DEFAULT NULL, -> description_short
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    
+    """
+
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     description_short = models.CharField(max_length=500)
@@ -58,24 +98,27 @@ class Race(models.Model):
     def __str__(self):
         return self.name
 
-# equivalent of invTypes
-#
-# implemented:
-# "typeID" integer NOT NULL, -> pk
-# "groupID" integer DEFAULT NULL, -> group
-# "typeName" varchar(100) DEFAULT NULL, -> name
-# "description" varchar(3000) DEFAULT NULL, -> description
-# "mass" double DEFAULT NULL, -> mass
-# "volume" double DEFAULT NULL, -> volume
-# "capacity" double DEFAULT NULL, -> capacity
-# "portionSize" integer DEFAULT NULL, -> portion_size
-# "raceID" integer DEFAULT NULL, -> race
-# "basePrice" decimal(19,4) DEFAULT NULL, -> base_price
-# "published" integer DEFAULT NULL, -> published
-# "marketGroupID" integer DEFAULT NULL, -> marketgroup
-# not implemented
-# "chanceOfDuplicating" double DEFAULT NULL,
 class InvType(models.Model):
+    """ Equivalent of invTypes
+    
+    "typeID" integer NOT NULL, -> pk
+    "groupID" integer DEFAULT NULL, -> group
+    "typeName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(3000) DEFAULT NULL, -> description
+    "mass" double DEFAULT NULL, -> mass
+    "volume" double DEFAULT NULL, -> volume
+    "capacity" double DEFAULT NULL, -> capacity
+    "portionSize" integer DEFAULT NULL, -> portion_size
+    "raceID" integer DEFAULT NULL, -> race
+    "basePrice" decimal(19,4) DEFAULT NULL, -> base_price
+    "published" integer DEFAULT NULL, -> published
+    "marketGroupID" integer DEFAULT NULL, -> marketgroup
+    
+    not implemented
+    "chanceOfDuplicating" double DEFAULT NULL,
+    
+    """
+
     name = models.CharField(max_length=100)
     invgroup = models.ForeignKey(InvGroup)
     marketgroup = models.ForeignKey(MarketGroup, null=True)
@@ -96,37 +139,40 @@ class Constellation(models.Model):
     name = models.CharField(max_length=100)
     # TODO
 
-# equivalent of mapSolarSystems
-#
-# implemented:
-# "regionID" integer DEFAULT NULL, -> region
-# "constellationID" integer DEFAULT NULL, -> constellation
-# "solarSystemID" integer NOT NULL, -> pk
-# "solarSystemName" varchar(100) DEFAULT NULL, -> name
-# "x" double DEFAULT NULL, -> x
-# "y" double DEFAULT NULL, -> y
-# "z" double DEFAULT NULL, -> z
-# "xMin" double DEFAULT NULL, -> x_min
-# "xMax" double DEFAULT NULL, -> x_max
-# "yMin" double DEFAULT NULL, -> y_min
-# "yMax" double DEFAULT NULL, -> y_max
-# "zMin" double DEFAULT NULL, -> z_min
-# "zMax" double DEFAULT NULL, -> z_max
-# "luminosity" double DEFAULT NULL, -> luminosity
-# "border" integer DEFAULT NULL, -> border
-# "fringe" integer DEFAULT NULL, -> fringe
-# "corridor" integer DEFAULT NULL, -> corridor
-# "hub" integer DEFAULT NULL, -> hub
-# "international" integer DEFAULT NULL, -> international
-# "regional" integer DEFAULT NULL, -> regional
-# "constellation" integer DEFAULT NULL, -> constellation
-# "security" double DEFAULT NULL, -> security
-# "securityClass" varchar(2) DEFAULT NULL, -> security_class
-# "radius" double DEFAULT NULL, -> radius
-# not implemented:
-# "sunTypeID" integer DEFAULT NULL,
-# "factionID" integer DEFAULT NULL,
 class SolarSystem(models.Model):
+    """ Equivalent of mapSolarSystems
+    
+    implemented:
+    "regionID" integer DEFAULT NULL, -> region
+    "constellationID" integer DEFAULT NULL, -> constellation
+    "solarSystemID" integer NOT NULL, -> pk
+    "solarSystemName" varchar(100) DEFAULT NULL, -> name
+    "x" double DEFAULT NULL, -> x
+    "y" double DEFAULT NULL, -> y
+    "z" double DEFAULT NULL, -> z
+    "xMin" double DEFAULT NULL, -> x_min
+    "xMax" double DEFAULT NULL, -> x_max
+    "yMin" double DEFAULT NULL, -> y_min
+    "yMax" double DEFAULT NULL, -> y_max
+    "zMin" double DEFAULT NULL, -> z_min
+    "zMax" double DEFAULT NULL, -> z_max
+    "luminosity" double DEFAULT NULL, -> luminosity
+    "border" integer DEFAULT NULL, -> border
+    "fringe" integer DEFAULT NULL, -> fringe
+    "corridor" integer DEFAULT NULL, -> corridor
+    "hub" integer DEFAULT NULL, -> hub
+    "international" integer DEFAULT NULL, -> international
+    "regional" integer DEFAULT NULL, -> regional
+    "constellation" integer DEFAULT NULL, -> constellation
+    "security" double DEFAULT NULL, -> security
+    "securityClass" varchar(2) DEFAULT NULL, -> security_class
+    "radius" double DEFAULT NULL, -> radius
+    
+    not implemented:
+    "sunTypeID" integer DEFAULT NULL,
+    "factionID" integer DEFAULT NULL,
+    """
+
     name = models.CharField(max_length=100)
     region = models.ForeignKey(Region)
     constellation = models.ForeignKey(Constellation)
