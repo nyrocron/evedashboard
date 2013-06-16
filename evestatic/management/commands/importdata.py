91b359df5e924d28b88610312706ffe9f40aa040
Florian Tautz <dev@nyronet.de>
2013-06-16 14:42:09 +0200

    first shot at adding stations to evestatic and import optimization

--- evestatic/management/commands/importdata.py
+++ evestatic/management/commands/importdata.py
@@ -1,9 +1,13 @@
 from django.core.management.base import NoArgsCommand
-from django.db import connections
+from django.db import connections, transaction
 
-from evestatic.models import Race, Faction
-from evestatic.models import MarketGroup, InvCategory, InvGroup, InvType
+from eve import settings
+
+from evestatic.models import InvName, InvCategory, InvGroup, InvType
+from evestatic.models import MarketGroup
+from evestatic.models import NPCCorporation, Race, Faction
 from evestatic.models import Region, Constellation, SolarSystem
+from evestatic.models import Station
 
 class Command(NoArgsCommand):
     args = ''
@@ -13,50 +17,41 @@ class Command(NoArgsCommand):
     _db_static = connections['evestatic']
     
     def handle_noargs(self, **options):
-        # chr
-        self._import_race()
-        self._import_faction()
-        
         # inv
+        self._import_invname()
         self._import_marketgroup()
         self._import_invcategory()
         self._import_invgroup()
         self._import_invtype()
         
-        #map
+        # chr
+        self._import_npc_corporation()
+        self._import_race()
+        self._import_faction()
+        
+        # map
         self._import_region()
         self._import_constellation()
         self._import_solarsystem()
         self._import_jumps()
+        self._import_map_denormalize()
+        
+        # sta
+        self._import_station()
         
         self.stdout.write("Static data import done.")
     
-    def _import_race(self):
-        """ Import from chrRaces table to Race model."""
-        self._import_table_data('chrRaces', Race, [
-            ('raceID', 'pk', None),
-            ('raceName', 'name', None),
-            ('description', 'description', _string_null_to_empty),
-            ('shortDescription', 'description_short', _string_null_to_empty),
-        ])
-    
-    def _import_faction(self):
-        """Import data from chrFactions table to Faction model."""
-        self._import_table_data('chrFactions', Faction, [
-            ('factionID', 'pk', None),
-            ('factionName', 'name', None),
-            #('solarSystemID', 'hq_system_id', None),
-            ('description', 'description', _string_null_to_empty),
-            #('corporationID', 'corporation', None),
-            ('sizeFactor', 'size_factor', None),
-            ('stationCount', 'station_count', None),
-            ('stationSystemCount', 'station_system_count', None),
+    def _import_invname(self, ):
+        """Import data from invNames to InvName model."""
+        self._import_table_data('invNames', InvName, [
+            ('itemID', 'id', None),
+            ('itemName', 'name', None)
         ])
     
     def _import_marketgroup(self):
         """Import from invMarketGroups table to MargetGroup model."""
         self._import_table_data('invMarketGroups', MarketGroup, [
-            ('marketGroupID', 'pk', None),
+            ('marketGroupID', 'id', None),
             ('parentGroupID', 'parent_id', None),
             ('marketGroupName', 'name', None),
             ('description', 'description', _string_null_to_empty),
@@ -66,7 +61,7 @@ class Command(NoArgsCommand):
     def _import_invcategory(self):
         """Import from invCategories table to InvCategory model."""
         self._import_table_data('invCategories', InvCategory, [
-            ('categoryID', 'pk', None),
+            ('categoryID', 'id', None),
             ('categoryName', 'name', None),
             ('description', 'description', _string_null_to_empty),
             ('published', 'is_published', _int_to_bool),
@@ -75,7 +70,7 @@ class Command(NoArgsCommand):
     def _import_invgroup(self):
         """Import from invGroups table to InvGroup model."""
         self._import_table_data('invGroups', InvGroup, [
-            ('groupID', 'pk', None),
+            ('groupID', 'id', None),
             ('categoryID', 'invcategory_id', None),
             ('groupName', 'name', None),
             ('description', 'description', _string_null_to_empty),
@@ -91,7 +86,7 @@ class Command(NoArgsCommand):
     def _import_invtype(self):
         """Import from invTypes table to InvTypes model."""
         self._import_table_data('invTypes', InvType, [
-            ('typeID', 'pk', None),
+            ('typeID', 'id', None),
             ('typeName', 'name', None),
             ('groupID', 'invgroup_id', None),
             ('marketGroupID', 'marketgroup_id', None),
@@ -104,10 +99,46 @@ class Command(NoArgsCommand):
             ('published', 'is_published', _int_to_bool),
         ])
     
+    def _import_npc_corporation(self):
+        """Import data from crpNPCCorporations to NPCCorporation model."""
+        self._import_table_data('crpNPCCorporations', NPCCorporation, [
+            ('corporationID', 'id', None),
+            ('factionID', 'faction_id', None),
+            ('solarSystemID', 'solarsystem_id', None),
+            ('description', 'description', _string_null_to_empty),
+            ('size', 'size', _string_null_to_empty),
+            ('extent', 'extent', _string_null_to_empty),
+            ('sizeFactor', 'size_factor', None),
+            ('stationCount', 'station_count', _int_null_to_zero),
+            ('stationSystemCount', 'station_system_count', _int_null_to_zero),
+        ])
+    
+    def _import_race(self):
+        """Import from chrRaces table to Race model."""
+        self._import_table_data('chrRaces', Race, [
+            ('raceID', 'id', None),
+            ('raceName', 'name', None),
+            ('description', 'description', _string_null_to_empty),
+            ('shortDescription', 'description_short', _string_null_to_empty),
+        ])
+    
+    def _import_faction(self):
+        """Import data from chrFactions table to Faction model."""
+        self._import_table_data('chrFactions', Faction, [
+            ('factionID', 'id', None),
+            ('factionName', 'name', None),
+            ('solarSystemID', 'hq_system_id', None),
+            ('description', 'description', _string_null_to_empty),
+            ('corporationID', 'main_corporation_id', None),
+            ('sizeFactor', 'size_factor', None),
+            ('stationCount', 'station_count', None),
+            ('stationSystemCount', 'station_system_count', None),
+        ])
+    
     def _import_region(self):
         """Import data from mapRegions table to Region model."""
         self._import_table_data('mapRegions', Region, [
-            ('regionID', 'pk', None),
+            ('regionID', 'id', None),
             ('regionName', 'name', None),
             ('factionID', 'faction_id', None),
             ('radius', 'radius', None),
@@ -125,7 +156,7 @@ class Command(NoArgsCommand):
     def _import_constellation(self):
         """Import data from mapConstellations to Constellation model"""
         self._import_table_data('mapConstellations', Constellation, [
-            ('constellationID', 'pk', None),
+            ('constellationID', 'id', None),
             ('constellationName', 'name', None),
             ('regionID', 'region_id', None),
             ('factionID', 'faction_id', None),
@@ -144,7 +175,7 @@ class Command(NoArgsCommand):
     def _import_solarsystem(self):
         """Import data from mapSolarSystems table to SolarSystem model."""
         self._import_table_data('mapSolarSystems', SolarSystem, [
-            ('solarSystemID', 'pk', None),
+            ('solarSystemID', 'id', None),
             ('solarSystemName', 'name', None),
             ('regionID', 'region_id', None),
             ('constellationID', 'constellation_id', None),
@@ -192,6 +223,45 @@ class Command(NoArgsCommand):
             system_to = SolarSystem.objects.get(pk=row[1])
             system_from.jumps.add(system_to)
     
+    def _import_map_denormalize(self):
+        """Import data from mapDenormalize to MapDenormalize model."""
+        self._import_table_data('mapDenormalize', MapDenormalize, [
+            ('itemID', 'id', None),
+            ('itemName', 'name', None),
+            ('typeID', 'inv_type_id', None),
+            ('groupID', 'inv_group_id', None),
+            ('solarSystemID', 'solarsystem_id', None),
+            ('constellationID', 'constellation_id', None),
+            ('regionID', 'region_id', None),
+            ('orbitID', 'orbit_id', None),
+            ('celestialIndex', 'celestial_index', None),
+            ('orbitIndex', 'orbit_index', None),
+            ('security', 'security', None),
+            ('radius', 'radius', None),
+            ('x', 'x', None),
+            ('y', 'y', None),
+            ('z', 'z', None),
+        ])
+    
+    def _import_station(self):
+        """Import data from staStations to Station model."""
+        self._import_table_data('staStations', Station, [
+            ('stationID', 'station_id', None),
+            ('stationName', 'name', None),
+            ('stationTypeID', 'station_type_id', None),
+            ('corporationID', 'corporation_id', None),
+            ('solarSystemID', 'solarsystem_id', None),
+            ('constellationID', 'constellation_id', None),
+            ('regionID', 'region_id', None),
+            ('security', 'security', None),
+            ('maxShipVolumeDockable', 'max_dock_volume', None),
+            ('reprocessingEfficiency', 'reprocessing_efficiency', None),
+            ('reprocessingStationsTake', 'reprocessing_take', None),
+            ('x', 'x', None),
+            ('y', 'y', None),
+            ('z', 'z', None),
+        ])
+    
     def _import_table_data(self, static_table, model, col_map):
         """ Import data from a static db table to a model"""
         # only import if there are no values in the table
@@ -204,10 +274,28 @@ class Command(NoArgsCommand):
                           (static_table, model.__name__))
                           #ending='')
         
-        # query static db
         cursor_static = self._db_static.cursor()
-        cursor_static.execute("SELECT " + ",".join([x[0] for x in col_map]) +
-                              " FROM " + static_table)
+        static_cols = ",".join([x[0] for x in col_map])
+        cursor_static.execute("SELECT %s FROM %s" %
+                              (static_cols, static_table))
+        
+        # if there are no transformations to be applied, import data via SQL
+        if (all(item[2] is None for item in col_map) and
+            settings.DATABASES['default']['ENGINE'] ==
+                'django.db.backends.sqlite3' and
+            settings.DATABASES['evestatic']['ENGINE'] ==
+                'django.db.backends.sqlite3'):
+            self.stdout.write("using direct import...")
+            model_table = model._meta.db_table
+            model_cols = ",".join([model._meta.get_field(x[1]).column
+                                   for x in col_map])
+            cursor_static.execute("ATTACH '%s' AS eve" %
+                                  settings.DATABASES['default']['NAME'])
+            cursor_static.execute("INSERT INTO eve.%s (%s) SELECT %s FROM %s" %
+                                  (model._meta.db_table, model_cols,
+                                   static_cols, static_table))
+            transaction.commit_unless_managed()
+            return
                 
         # from sql result create models, apply transform if there is any,
         # then save the created object
@@ -219,12 +307,18 @@ class Command(NoArgsCommand):
                 else:
                     model_values[col_map[i][1]] = row[i]
             model(**model_values).save()
+
+def _int_to_bool(value):
+    return value == 1
     
+def _int_null_to_zero(value):
+    if value is None:
+        return 0
+    else:
+        return value
+
 def _string_null_to_empty(value):
     if value is None:
         return ""
     else:
         return value
-
-def _int_to_bool(value):
-    return value == 1
--- evestatic/models.py
+++ evestatic/models.py
@@ -1,51 +1,15 @@
 from django.db import models
 
-class Race(models.Model):
-    """ Equivalent of chrRaces
+class InvName(models.Model):
+    """ Equivalent of invNames
     
-    "raceID" integer NOT NULL, -> pk
-    "raceName" varchar(100) DEFAULT NULL, -> name
-    "description" varchar(1000) DEFAULT NULL, -> description
-    "shortDescription" varchar(500) DEFAULT NULL, -> description_short
-    
-    not implemented:
-    "iconID" integer DEFAULT NULL,
-    
-    """
-
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
-    description_short = models.CharField(max_length=500)
-    
-    def __str__(self):
-        return self.name
-
-class Faction(models.Model):
-    """ Equivalent of chrFactions
-    
-    "factionID" integer NOT NULL, -> pk
-    "factionName" varchar(100) DEFAULT NULL, -> name
-    "description" varchar(1000) DEFAULT NULL, -> description
-    "sizeFactor" double DEFAULT NULL, -> size_factor
-    "stationCount" integer DEFAULT NULL, -> station_count
-    "stationSystemCount" integer DEFAULT NULL, -> station_system_count
-    
-    not implemented:
-    "iconID" integer DEFAULT NULL,
-    "militiaCorporationID" integer DEFAULT NULL,
-    "raceIDs" integer DEFAULT NULL,
-    "solarSystemID" integer DEFAULT NULL, -> hq_system
-    "corporationID" integer DEFAULT NULL, -> corporation
+    "itemID" integer NOT NULL, -> pk
+    "itemName" varchar(200) NOT NULL, -> name
     
     """
     
-    name = models.CharField(max_length=100)
-    #hq_system = models.ForeignKey('SolarSystem')
-    description = models.CharField(max_length=1000)
-    #corporation = models.ForeignKey(Corporation)
-    size_factor = models.FloatField()
-    station_count = models.IntegerField()
-    station_system_count = models.IntegerField()
+    id = models.IntegerField(primary_key=True, db_column='id')
+    name = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
@@ -64,6 +28,7 @@ class InvCategory(models.Model):
     
     """
     
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=3000)
     is_published = models.BooleanField()
@@ -92,6 +57,7 @@ class InvGroup(models.Model):
     
     """
     
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     invcategory = models.ForeignKey(InvCategory)
     description = models.CharField(max_length=3000)
@@ -119,6 +85,8 @@ class MarketGroup(models.Model):
     "iconID" integer DEFAULT NULL,
     
     """
+    
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', null=True)
     description = models.CharField(max_length=3000)
@@ -148,10 +116,11 @@ class InvType(models.Model):
     
     """
 
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     invgroup = models.ForeignKey(InvGroup)
     marketgroup = models.ForeignKey(MarketGroup, null=True)
-    race = models.ForeignKey(Race, null=True)
+    race = models.ForeignKey('Race', null=True)
     description = models.CharField(max_length=3000)
     mass = models.FloatField(null=True)
     volume = models.FloatField(null=True)
@@ -166,6 +135,116 @@ class InvType(models.Model):
     @staticmethod
     def get_type_name(type_id):
         return InvType.objects.get(pk=type_id).name
+
+class Race(models.Model):
+    """ Equivalent of chrRaces
+    
+    "raceID" integer NOT NULL, -> pk
+    "raceName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(1000) DEFAULT NULL, -> description
+    "shortDescription" varchar(500) DEFAULT NULL, -> description_short
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    
+    """
+
+    id = models.IntegerField(primary_key=True, db_column='id')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    description_short = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+
+class NPCCorporation(models.Model):
+    """ Equivalent of chrNPCCorporations
+    
+    "corporationID" integer NOT NULL, -> pk
+    "factionID" integer DEFAULT NULL, -> faction (ref Faction)
+    "solarSystemID" integer DEFAULT NULL, -> solarsystem (ref SolarSystem)
+    "description" varchar(4000) DEFAULT NULL, -> description
+    "size" char(1) DEFAULT NULL, -> size
+    "extent" char(1) DEFAULT NULL, -> extent
+    "sizeFactor" double DEFAULT NULL, -> size_factor
+    "stationCount" integer DEFAULT NULL, -> station_count
+    "stationSystemCount" integer DEFAULT NULL, -> station_system_count
+    
+    not implemented:
+    "investorID1" integer DEFAULT NULL,
+    "investorShares1" integer DEFAULT NULL,
+    "investorID2" integer DEFAULT NULL,
+    "investorShares2" integer DEFAULT NULL,
+    "investorID3" integer DEFAULT NULL,
+    "investorShares3" integer DEFAULT NULL,
+    "investorID4" integer DEFAULT NULL,
+    "investorShares4" integer DEFAULT NULL,
+    "friendID" integer DEFAULT NULL, 
+    "enemyID" integer DEFAULT NULL,
+    "publicShares" integer DEFAULT NULL,
+    "initialPrice" integer DEFAULT NULL,
+    "minSecurity" double DEFAULT NULL,
+    "iconID" integer DEFAULT NULL,
+    "scattered" integer DEFAULT NULL, -> is_scattered
+    "fringe" integer DEFAULT NULL, -> is_fringe
+    "corridor" integer DEFAULT NULL, -> is_corridor
+    "hub" integer DEFAULT NULL, -> is_hub
+    "border" integer DEFAULT NULL, -> is_border
+    
+    """
+    
+    id = models.IntegerField(primary_key=True, db_column='id')
+    faction = models.ForeignKey('Faction')
+    solarsystem = models.ForeignKey('SolarSystem')
+    description = models.CharField(max_length=4000, null=True)
+    size = models.CharField(max_length=1)
+    extent = models.CharField(max_length=1)
+    size_factor = models.FloatField(null=True)
+    station_count = models.FloatField()
+    station_system_count = models.FloatField()
+    
+    def __str__(self):
+        return self.name()
+    
+    def name(self):
+        """Get the name of this corporation"""
+        try:
+            return self._name
+        except AttributeError:
+            self._name = InvName.objects.get(pk=self.pk).name
+            return self._name
+
+class Faction(models.Model):
+    """ Equivalent of chrFactions
+    
+    "factionID" integer NOT NULL, -> pk
+    "factionName" varchar(100) DEFAULT NULL, -> name
+    "description" varchar(1000) DEFAULT NULL, -> description
+    "sizeFactor" double DEFAULT NULL, -> size_factor
+    "stationCount" integer DEFAULT NULL, -> station_count
+    "stationSystemCount" integer DEFAULT NULL, -> station_system_count
+    
+    not implemented:
+    "iconID" integer DEFAULT NULL,
+    "militiaCorporationID" integer DEFAULT NULL,
+    "raceIDs" integer DEFAULT NULL,
+    "solarSystemID" integer DEFAULT NULL, -> hq_system
+    "corporationID" integer DEFAULT NULL, -> corporation
+    
+    """
+    
+    id = models.IntegerField(primary_key=True, db_column='id')
+    name = models.CharField(max_length=100)
+    hq_system = models.ForeignKey('SolarSystem')
+    description = models.CharField(max_length=1000)
+    main_corporation = models.ForeignKey(NPCCorporation,
+                                         related_name='main_corporation')
+    size_factor = models.FloatField()
+    station_count = models.IntegerField()
+    station_system_count = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
 
 class Region(models.Model):
     """ Equivalent of mapRegions
@@ -186,6 +265,7 @@ class Region(models.Model):
     
     """
     
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     faction = models.ForeignKey(Faction, null=True)
     radius = models.FloatField()
@@ -222,6 +302,7 @@ class Constellation(models.Model):
     
     """
     
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     region = models.ForeignKey(Region)
     faction = models.ForeignKey(Faction, null=True)
@@ -272,6 +353,7 @@ class SolarSystem(models.Model):
     "factionID" integer DEFAULT NULL,
     """
 
+    id = models.IntegerField(primary_key=True, db_column='id')
     name = models.CharField(max_length=100)
     region = models.ForeignKey(Region)
     constellation = models.ForeignKey(Constellation)
@@ -299,3 +381,119 @@ class SolarSystem(models.Model):
     
     def __str__(self):
         return self.name
+
+class MapDenormalize(models.Model):
+    """ Equivalent of mapDenormalize
+    
+    "itemID" integer NOT NULL, -> pk
+    "itemName" varchar(100) DEFAULT NULL, -> name
+    "typeID" integer DEFAULT NULL, -> inv_type (ref InvType)
+    "groupID" integer DEFAULT NULL, -> inv_group (ref InvGroup)
+    "solarSystemID" integer DEFAULT NULL, -> solarsystem (ref SolarSystem)
+    "constellationID" integer DEFAULT NULL, -> constellation (ref Constellation)
+    "regionID" integer DEFAULT NULL, -> region (ref Region)
+    "orbitID" integer DEFAULT NULL, -> orbit (ref self)
+    "celestialIndex" integer DEFAULT NULL, -> celestial_index
+    "orbitIndex" integer DEFAULT NULL, -> orbit_index
+    "security" double DEFAULT NULL, -> security
+    "radius" double DEFAULT NULL, -> radius
+    "x" double DEFAULT NULL, -> x
+    "y" double DEFAULT NULL, -> y
+    "z" double DEFAULT NULL, -> z
+
+    """
+    
+    id = models.IntegerField(primary_key=True, db_column='id')
+    name = models.CharField(max_length=100)
+    inv_type = models.ForeignKey(InvType)
+    inv_group = models.ForeignKey(InvGroup)
+    solarsystem = models.ForeignKey(SolarSystem)
+    constellation = models.ForeignKey(Constellation)
+    region = models.ForeignKey(Region)
+    orbit = models.ForeignKey('self')
+    celestial_index = models.IntegerField()
+    orbit_index = models.IntegerField()
+    security = models.FloatField(null=True)
+    radius = models.FloatField()
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+
+#class StationType(models.Model):
+#    """ Equivalent of staStationTypes
+#    
+#    "stationTypeID" integer NOT NULL,
+#    "dockEntryX" double DEFAULT NULL,
+#    "dockEntryY" double DEFAULT NULL,
+#    "dockEntryZ" double DEFAULT NULL,
+#    "dockOrientationX" double DEFAULT NULL,
+#    "dockOrientationY" double DEFAULT NULL,
+#    "dockOrientationZ" double DEFAULT NULL,
+#    "operationID" integer DEFAULT NULL,
+#    "officeSlots" integer DEFAULT NULL,
+#    "reprocessingEfficiency" double DEFAULT NULL,
+#    "conquerable" integer DEFAULT NULL,
+#    
+#    """
+#    
+#    name = models.CharField(max_length=100)
+#    
+#    def __str__(self):
+#        return self.name
+
+class Station(models.Model):
+    """ Equivalent of staStations
+    
+    "stationID" integer NOT NULL, -> station_id (ref MapDenormalize)
+    "stationName" varchar(100) DEFAULT NULL, -> name
+    #"stationTypeID" integer DEFAULT NULL, -> station_type (ref StationType)
+    "stationTypeID" integer DEFAULT NULL, -> station_type (ref InvType)
+    "corporationID" integer DEFAULT NULL, -> corporation (ref Corporation)
+    "solarSystemID" integer DEFAULT NULL, -> solarsystem (ref SolarSystem)
+    "constellationID" integer DEFAULT NULL, -> constellation (ref Constellation)
+    "regionID" integer DEFAULT NULL, -> region (ref Region)
+    "security" integer DEFAULT NULL, -> security
+    "maxShipVolumeDockable" double DEFAULT NULL, -> max_dock_volume
+    "reprocessingEfficiency" double DEFAULT NULL, -> reprocessing_efficiency
+    "reprocessingStationsTake" double DEFAULT NULL, -> reprocessing_take
+    "x" double DEFAULT NULL, -> x
+    "y" double DEFAULT NULL, -> y
+    "z" double DEFAULT NULL, -> z
+    
+    not implemented:
+    "dockingCostPerVolume" double DEFAULT NULL,
+    "officeRentalCost" integer DEFAULT NULL,
+    "operationID" integer DEFAULT NULL,
+    "reprocessingHangarFlag" integer DEFAULT NULL,
+    
+    """
+    
+    station_id = models.ForeignKey(MapDenormalize, primary_key=True,
+                                   db_column='station_id')
+    name = models.CharField(max_length=100)
+    #station_type = models.ForeignKey(StationType)
+    station_type = models.ForeignKey(InvType)
+    corporation = models.ForeignKey(NPCCorporation)
+    solarsystem = models.ForeignKey(SolarSystem)
+    constellation = models.ForeignKey(Constellation)
+    region = models.ForeignKey(Region)
+    security = models.FloatField()
+    max_dock_volume = models.FloatField()
+    reprocessing_efficiency = models.FloatField()
+    reprocessing_take = models.FloatField()
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+    
+    def type_name(self):
+        try:
+            return self._type_name
+        except AttributeError:
+            self._type_name = InvTypes.objects.get(pk=self.pk).name
+            return self._type_name
